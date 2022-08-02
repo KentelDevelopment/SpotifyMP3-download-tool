c5a65ab981374e94b60ee1bfe2aec59e1eeb1e9f
Efe Akaröz <69296379+efeakaroz13@users.noreply.github.com>
2022-08-02 17:46:23 +0300

    ascii art update

--- musicdownloader_commandline.py
+++ musicdownloader_commandline.py
@@ -5,6 +5,21 @@ from pprint import pprint
 from colorama import Fore, Back, Style
 from searchyt import Downloader
 
+print(Fore.GREEN,"""
+    888b     d888 8888888b.   .d8888b.  8888888888 Y88b   d88P
+    8888b   d8888 888   Y88b d88P  Y88b 888         Y88b d88P
+    88888b.d88888 888    888      .d88P 888          Y88o88P
+    888Y88888P888 888   d88P     8888"  8888888       Y888P
+    888 Y888P 888 8888888P"       "Y8b. 888            888
+    888  Y8P  888 888        888    888 888            888
+    888   "   888 888        Y88b  d88P 888            888
+    888       888 888         "Y8888P"  888            888
+    
+""")
+
+print(Fore.RESET)
+print("https://github.com/efeakaroz13")
+print("Author: ",Fore.CYAN,"Efe Akaröz",Fore.RESET)
 theurl = input("? | Enter a Spotify playlist URL:")
 
 try:
